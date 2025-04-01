6ea1c7f2d9452f4db44d0843185538273152c637
崔星星 <cuixingxing150@gmail.com>
2025-04-01 10:57:04 +0800

    Update imageInterp.m

--- imageInterp.m
+++ imageInterp.m
@@ -5,27 +5,28 @@ arguments
     mapY 
     options.FillValues (1,1) double {mustBeInRange(options.FillValues,0,255)}=0
     options.BorderMode (1,1) string {mustBeMember(options.BorderMode,["BORDER_CONSTANT","BORDER_REPLICATE","BORDER_REFLECT"])} = "BORDER_CONSTANT"
+    options.SmoothEdges logical = false; % 是否对边缘反锯齿/光滑处理
 end
 [h,w,~] = size(inImage);
 
 switch options.BorderMode
     case "BORDER_CONSTANT"
         % 使用固定值填充边界
-        outImage = images.internal.interp2d(inImage,mapX,mapY,"linear",options.FillValues,false);
+        outImage = images.internal.interp2d(inImage,mapX,mapY,"linear",options.FillValues,options.SmoothEdges);
         
     case "BORDER_REPLICATE"
         % 限制坐标到图像范围内，实现边界复制
         mapX_clamped = max(1, min(w, mapX));
         mapY_clamped = max(1, min(h, mapY));
         % out = interp2(1:w, 1:h, inImage, mapX_clamped, mapY_clamped, 'linear');
-        outImage = images.internal.interp2d(inImage,mapX_clamped,mapY_clamped,"linear",options.FillValues,false);
+        outImage = images.internal.interp2d(inImage,mapX_clamped,mapY_clamped,"linear",options.FillValues,options.SmoothEdges);
         
     case "BORDER_REFLECT"
         % 反射坐标处理
         mapX_reflect = reflectCoordinates(mapX, w);
         mapY_reflect = reflectCoordinates(mapY, h);
         % out = interp2(1:w, 1:h, inImage, mapX_reflect, mapY_reflect, 'linear');
-        outImage = images.internal.interp2d(inImage,mapX_reflect,mapY_reflect,"linear",options.FillValues,false);
+        outImage = images.internal.interp2d(inImage,mapX_reflect,mapY_reflect,"linear",options.FillValues,options.SmoothEdges);
     otherwise
         error('Invalid border mode: %s', borderMode);
 end
